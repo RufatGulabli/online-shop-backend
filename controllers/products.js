dcc82a92e194d89ada3c5bee7cebc3805a71f872
RufatGulabli <gulabli.rufat@gmail.com>
2019-05-20 14:21:44 +0400

    table-remove-update

--- controllers/products.js
+++ controllers/products.js
@@ -39,11 +39,14 @@ router.get("/", [verifyToken, verifyAdmin], async (req, res, next) => {
     const orderBy = req.query["sortOrder"] || "asc";
     const limit = parseInt(req.query["pageSize"]) || 10;
     const offset = parseInt(req.query["pageNumber"]) || 1;
+    const filter = req.query["filter"] || "";
     const products = await db_connection("products")
       .select("*")
+      .where("title", "ilike", "%".concat(filter).concat("%"))
       .orderBy(sortColumn, orderBy)
       .limit(limit)
       .offset((offset - 1) * limit);
+    console.log(products);
     res.status(200).json(products);
   } catch (err) {
     next(err);
@@ -66,7 +69,7 @@ router.get("/:id", async (req, res, next) => {
     let param = req.params["id"];
     const _id = parseInt(param);
     if (isNaN(_id)) {
-      throw new Error(`Invalid URL Parameter: ${param}`);
+      res.status(400).json({ error: 1, body: "ID must be a number" });
     }
     const products = await db_connection("products")
       .select("*")
@@ -76,6 +79,24 @@ router.get("/:id", async (req, res, next) => {
     res.status(200).json(products);
   } catch (ex) {
     next(ex);
+  }
+});
+
+router.delete("/:id", [verifyToken, verifyAdmin], async (req, res, next) => {
+  try {
+    let productId = parseInt(req.params["id"]);
+    if (isNaN(productId)) {
+      res.status(400).json({ error: 1, body: "ID must be a number" });
+    }
+    let rowsAffected = await db_connection("products")
+      .where("id", productId)
+      .delete();
+    if (!rowsAffected) {
+      res.status(400).json("There is not a product qith a given ID");
+    }
+    res.status(200).json(rowsAffected);
+  } catch (err) {
+    next(err);
   }
 });
 
